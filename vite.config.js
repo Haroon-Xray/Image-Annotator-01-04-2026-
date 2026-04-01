6a965b8ae6edc2f9a9417db8cd461283705df9e0
Haroon Rasheed <haroontech@hotmail.com>
2026-04-01 14:30:28 +0500

    Refactor CSS and JSX formatting for consistency; update Vite config indentation

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: false,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-  },
+   plugins: [react()],
+   server: {
+      port: 5173,
+      strictPort: false,
+      open: true,
+   },
+   build: {
+      outDir: 'dist',
+      sourcemap: false,
+      minify: 'terser',
+   },
 })
